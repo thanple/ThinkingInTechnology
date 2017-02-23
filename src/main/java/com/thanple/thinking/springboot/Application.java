292cd7ca6488dbe3c2a79548b33ecb2f458db5c5
null47 <805685524@qq.com>
2017-02-24 00:27:43 +0800

    SpringBoot的一些总结: logback

--- src/main/java/com/thanple/thinking/springboot/Application.java
+++ src/main/java/com/thanple/thinking/springboot/Application.java
@@ -1,5 +1,9 @@
 package com.thanple.thinking.springboot;
 
+import com.thanple.thinking.springboot.config.MyConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,12 +16,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+    Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private MyConfig myConfig;
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     public void run(String... strings) throws Exception {
-
+        log.info("MyConfig Value{},{}",myConfig.getValue1(),myConfig.getValue2());
     }
 }
--- src/main/java/com/thanple/thinking/springboot/config/MyConfig.java
+++ src/main/java/com/thanple/thinking/springboot/config/MyConfig.java
@@ -1,12 +1,23 @@
 package com.thanple.thinking.springboot.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by Thanple on 2017/1/4.
+ *
+ * 读取配置文件
  */
 
 @Component
+@Data
 public class MyConfig {
+
+    @Value("${MyConfig.value1}")
+    private String value1;
+
+    @Value("${MyConfig.value2}")
+    private String value2;
 
 }
